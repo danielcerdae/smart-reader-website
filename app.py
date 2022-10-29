e0c748d8d5aceb153b82525a290992a897ce67a9
Daniel Cerda <danielcerda.e@gmail.com>
2022-10-29 17:40:59 -0300

    Branch commited

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
+#from turtle import up
 import streamlit as st
-
+#import urllib2
+import requests
 import datetime
 import os
 import requests
 
-'''
-# Engineering Drawings Smart Reader
+st.markdown("""
+    # Engineering Drawings Smart Reader
 
-This front queries the Le Wagon [taxi fare model API]
-'''
+    ### A powerfull tool to retrieve data from engineering P&ID drawings
+
+    **bold** or *italic* text with [links](http://github.com/streamlit) and:
+    - bullet points
+""")
+URL = 'http://localhost:8000'
+
+uploaded_file = st.file_uploader("Upload a file")
+
+
+if uploaded_file:
+
+    st.write("Filename: ", uploaded_file.name)
+
+    if st.button('Send file'):
+        # print is visible in the server output, not in the page
+        print('button clicked!')
+        response = requests.post(f'{URL}/upload', files={'file':uploaded_file}, verify=False)
+        st.write(response)
+        #st.write('Further clicks are not visible but are executed')
+    else:
+        st.write('I was not clicked 😞')
